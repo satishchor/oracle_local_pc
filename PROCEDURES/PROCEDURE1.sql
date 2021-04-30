78c50338fb57179880558e8baae8ad0d96187508
unknown <chorghesatish8@outlook.com>
2021-04-30 13:31:04 +0530

    start testing git with oracle

--- PROCEDURES/PROCEDURE1.sql
+++ PROCEDURES/PROCEDURE1.sql
@@ -1,12 +1,16 @@
+---------------------------------------------------------------------------------------------------
+-- FUNCTION : To print the data in to console and Set variable.
+---------------------------------------------------------------------------------------------------
+
 CREATE OR REPLACE NONEDITIONABLE PROCEDURE PROCEDURE1 
 (
   PARAM1 IN VARCHAR2 
 ) AS 
 BEGIN
   
-  DECLARE PARAM2 VARCHAR2(30);
+  DECLARE PARAM2 VARCHAR2(20);
   BEGIN
---  SET PARAM2:= '3';
     DBMS_OUTPUT.PUT(', ');
+    PARAM2 := 'satish';
   END;
 END PROCEDURE1;
---
+++ Query_Practice/Simple Operations.sql
@@ -0,0 +1,12 @@
+Select * From Test1;
+
+insert into test1 values ('1234');
+commit;
+
+select * from ALL_TAB_MODIFICATIONS where ROWNUM < 20;
+select * from all_procedures where owner = '' and rownum < 20;
+
+SELECT *
+FROM USER_OBJECTS
+WHERE OBJECT_TYPE IN ('PROCEDURE','TABLE');
+
